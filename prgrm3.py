3e786687423c1ed3514c1e2e19e67efa99c0c873
shifanasherin <shiff553@gmail.com>
2026-07-06 02:41:29 -0700

    Delete prgrm3.py

--- prgrm3.py
+++
@@ -1,6 +0,0 @@
-def count_words(text):
-    words=text.split()
-    return len(words)
-user_input("enter a sentence or paragraph")
-total_words=count_words(user_input)
-print(f"total words: {total_words}")
