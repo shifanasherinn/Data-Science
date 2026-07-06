21b5aef3d2c8d6c359146d129b568805b37bf1ff
shifanasherin <shiff553@gmail.com>
2026-07-06 02:43:35 -0700

    Add files via upload

---
+++ prgrm3.py
@@ -0,0 +1,6 @@
+def count_words(text):
+    words=text.split()
+    return len(words)
+user_input=input("enter a sentence or paragraph")
+total_words=count_words(user_input)
+print(f"total words: {total_words}")
